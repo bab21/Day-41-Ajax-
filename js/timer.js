3c31c99517cfe9a0f98d574436a755b5f7c1a6bc
bab21 <2016061@iiitdmj.ac.in>
2020-11-23 12:50:19 +0530

    demonstrate asynchronous nature of Javascript

---
+++ js/timer.js
@@ -0,0 +1,13 @@
+function showTime(){
+    const date =new Date();
+    return date.getHours()+"Hrs: "+ date.getMinutes()+ "Mins: "+date.getSeconds()+" Secs ";
+
+}
+
+function showSessionExpire(){
+    console.log("Activity-B : Your Session expired at "+showTime());
+}
+
+console.log("Activity-A : Triggerring Activity -B at"+showTime());
+setTimeout(showSessionExpire,5000);
+console.log("Activity-A Tiggered Activity-B at "+showTime());
